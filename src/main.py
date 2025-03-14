38ab071700c51bff289b0400ea6d8b2aa7818ced
Bryce Fongvongsa <BryceF24@gmail.com>
2025-03-13 21:38:05 -0500

    renamed files

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
-import function_practice
+import utils
 from Dog import Dog
 from breeds import *
+import utils
 import hello_docker_image
 
 if __name__ == "__main__":
     print(hello_docker_image.print_message())
     
+    print('----------Dog class start -----------')
     taro = Pomeranian('Taro', 0)
     rui = Dog('Rui', 4)
     finn = Dog('Finn', 4)
     dogs = [taro, rui, finn]
     print(taro.bark())
     print(Dog.calculate_average_age(dogs))
+    print('----------Dog class over -----------')
     
