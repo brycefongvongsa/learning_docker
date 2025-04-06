4946d39c2b7684bf0d3d51e9d721644d7f33aa99
Bryce Fongvongsa <BryceF24@gmail.com>
2025-04-05 19:01:12 -0500

    Refactor dog data handling: convert CSV to JSON and update loading mechanism; remove dog_sheet.txt

--- src/main.py
+++ src/main.py
@@ -1,21 +1,36 @@
-import utils
 from Dog import Dog
 from breeds import *
 import utils
 import hello_docker_image
+import json
+import csv
 
 if __name__ == "__main__":
+
     print(hello_docker_image.print_message())
+    print("--------------------")
+    print("Welcome to the Dog Management System!" + '\n')
     
-    print('----------Dog class start -----------')
-    taro = Pomeranian('Taro', 0, 'BF')
-    rui = Dog('Rui', 4, "SF")
-    finn = Dog('Finn', 4, "CF")
-    miso = Dog('Miso', 4, 'AH')
-    dogs = [taro, rui, finn, miso]
-    print(taro.bark())
-    print('----------Dog class over -----------')
+    # Convert CSV to JSON
+    csv_file_path = '../data/dog_sheet.csv'
+    json_file_path = '../data/dog_sheet.json'
+    with open(csv_file_path, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        data = [row for row in csv_reader] # Read CSV data into a list of dictionaries
     
-    with open('../data/dog_sheet.txt', 'w') as f:
-        for dog in dogs:
-            f.write(dog.getName() + '|' + str(dog.getAge()) + '|' + dog.getOwner() + '\n')
+    with open(json_file_path, 'w') as json_file:
+        json.dump(data, json_file) # Write the list of dictionaries to a JSON file
+    
+    # Load dogs from JSON file
+    print("Loading dogs from JSON file...")
+    with open(json_file_path, 'r') as json_file:
+        json_data = json.load(json_file)
+        dogs = []
+        for row in json_data:
+            dog = Dog(name=row['Name'], age=int(row['Age']), owner=row['Owner'])
+            dogs.append(dog)
+    
+    print("\nAll dogs loaded successfully!")
+    for d in dogs:
+        print(f"Name: {d.getName()}, Age: {d.getAge()}, Owner: {d.getOwner()}")
+            
